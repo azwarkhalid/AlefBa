7a1355f59940480ba607ca82dc8e3e287db67b63
azwarkhalid <hello@azwarkhalid.com>
2013-01-13 21:00:40 -0500

    adding link to full version app

--- alefTrial/end.lua
+++ alefTrial/end.lua
@@ -18,6 +18,7 @@ end
 local function onDownloadButton( self, event) 
 	if event.phase == "ended" or event.phase == "cancelled" then
 		--system.openURL("https://play.google.com/store/apps/details?id=com.marqoomlabs.alefbafull095")
+		system.openURL("https://itunes.apple.com/au/app/alef-ba-arabic-alphabet-for/id590048145#")
 		storyboard.gotoScene( "home" )
 	end
 end
@@ -32,19 +33,17 @@ function scene:createScene( event )
 	elephant.x = display.contentWidth/2 - 120
 	elephant.y = display.contentHeight/2 + 210
 	
-	--speech = display.newImage("speech_bubble.png")
-	--speech.x = display.contentWidth/2 + 50
-	--speech.y = display.contentHeight/2 - 200	
+	speech = display.newImage("speech_bubble.png")
+	speech.x = display.contentWidth/2 + 50
+	speech.y = display.contentHeight/2 - 200	
 	
-	--download = display.newImage("download_button.png")
-	download = display.newImage("start.png")
+	download = display.newImage("download_button.png")
 	download.x = display.contentWidth/2
-	--download.y = display.contentHeight/2 + 450
-	download.y = display.contentHeight/2 - 120
+	download.y = display.contentHeight/2 + 450
 	
 	group:insert( background )
 	group:insert( elephant )
-	--group:insert( speech )
+	group:insert( speech )
 	group:insert( download )
 end
 
@@ -52,8 +51,8 @@ function scene:enterScene( event )
 	elephant.touch = onStartButtonTouch
 	elephant:addEventListener( "touch", elephant )
 	
-	--speech.touch = onDownloadButton
-	--speech:addEventListener( "touch", speech )
+	speech.touch = onDownloadButton
+	speech:addEventListener( "touch", speech )
 	
 	download.touch = onDownloadButton
 	download:addEventListener( "touch", download )
@@ -63,7 +62,7 @@ function scene:exitScene( event )
 	local group = self.view
 	
 	background:removeEventListener( "touch", elephant )
-	--background:removeEventListener( "touch", speech )
+	background:removeEventListener( "touch", speech )
 	background:removeEventListener( "touch", download )
 end
 
